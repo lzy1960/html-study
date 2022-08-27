b2cc6af3f444bd7fabd5298d0dd0255439b000da
lzy1960 <1960553899@qq.com>
2022-08-27 10:09:08 +0800

    shuffle优化

--- shuffle.js
+++ shuffle.js
@@ -1,13 +1,14 @@
-function shuffle (arr, size) {
+function shuffle (arr) {
   let index = -1, len = arr.length, lastIndex = len - 1
-  size = size || len
-  while (++index < size) {
+  while (++index < len) {
     let rand = index + Math.floor(Math.random() * lastIndex - index + 1)
     let value = arr[rand]
     arr[rand] = arr[index]
     arr[index] = value
   }
-  arr.length = size
   return arr
 }
-console.log(shuffle([ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 ]));
+function shuffle2 (arr) {
+  return arr.sort(() => Math.random() - 0.5)
+}
+console.log(shuffle([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]));
